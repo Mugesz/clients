5ac2a9065d373dcac0cefd467900689f3b2a6574
frekerzzzz <kannamugesh135@gmail.com>
2024-06-21 15:35:13 +0530

    dcadc

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -4,20 +4,35 @@ import { faSignOutAlt } from "@fortawesome/free-solid-svg-icons";
 
 const Navbar = ({ logout, userData }) => {
   return (
-    <nav className="navbar navbar-expand-lg navbar-light text-light fixed-top mt-0" style={{ backgroundColor: '#e8280b' }}>
+    <nav
+      className="navbar navbar-expand-lg navbar-light text-light fixed-top mt-0"
+      style={{ backgroundColor: "#e8280b" }}
+    >
       <div className="container">
         <a className="navbar-brand nav-clr" href="#">
           <b>GOOD NEWS</b>
         </a>
         <div className="collapse navbar-collapse" id="navbarNav"></div>
-        <div className="d-flex me-3">
+        <div className="d-flex align-items-center gap-2 justify-content-center me-3">
           <div className="me-3">
-            <h1>Welcome</h1>
-            <h5>{userData.displayName}</h5>
-            <button className="btn btn-light" onClick={logout}><FontAwesomeIcon icon={faSignOutAlt} style={{ color: "#262726" }} /></button>
+            <h4 className="">Welcome</h4>
+            <h6>{userData.displayName}</h6>
           </div>
           <div className="mt-0">
-            <img className="rounded-circle" style={{ height: "100px", width: "100px" }} src={userData.photoURL} alt="" />
+            <img
+              className="rounded-circle"
+              style={{ height: "70px", width: "70px" }}
+              src={userData.photoURL}
+              alt=""
+            />
+          </div>
+          <div className="">
+            <button className="btn btn-light" onClick={logout}>
+              <FontAwesomeIcon
+                icon={faSignOutAlt}
+                style={{ color: "#262726" }}
+              />
+            </button>
           </div>
         </div>
       </div>
